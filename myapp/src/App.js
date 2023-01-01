0a9ba5ed657179f9020e835559fa8402f928d391
sagir <capitaineibrahim@gmail.com>
2023-01-01 03:26:13 +0100

    State, setState and eventHandling

---
+++ myapp/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import State from "./State";
+
+export default function App() {
+  return (
+    <div>
+      <State count="0" />
+    </div>
+  );
+}
---
+++ myapp/src/State.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import "./style.css";
+export default class State extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+  handleDecrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  ReInitialize = () => {
+    this.setState({
+      count: 0,
+    });
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Count : {count}</h1>
+
+        <div className="buttonalign">
+          <button
+            onClick={this.handleIncrement}
+            disabled={count === 10 ? true : false}
+          >
+            +
+          </button>
+          <button
+            onClick={this.handleDecrement}
+            disabled={count === -10 ? true : false}
+          >
+            -
+          </button>
+          <button onClick={this.ReInitialize}>ReInitialize</button>
+        </div>
+      </div>
+    );
+  }
+}
