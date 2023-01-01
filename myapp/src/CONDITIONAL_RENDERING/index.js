aee27653b7800f91aa94484ef6aab271a2c66937
sagir <capitaineibrahim@gmail.com>
2023-01-01 05:02:26 +0100

    conditional rendering with short circuit methodn

--- myapp/src/CONDITIONAL_RENDERING/index.js
+++ myapp/src/CONDITIONAL_RENDERING/index.js
@@ -6,13 +6,13 @@ export default class CONDITIONAL_RENDERING extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: false,
+      isLoggedIn: true,
     };
   }
 
   render() {
     const { isLoggedIn } = this.state;
 
-    return <div>{isLoggedIn ? <HomePage /> : <Login />}</div>;
+    return <div>{isLoggedIn && <HomePage />}</div>;
   }
 }
